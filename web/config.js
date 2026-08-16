7f1b9c1df1f85c0248b11f9579f99e2d244b68c0
djarotsantoso2-dotcom <djarotsantoso2@gmail.com>
2026-08-16 15:06:14 +0000

    Connect frontend to Apps Script backend

--- web/config.js
+++ web/config.js
@@ -1,6 +1,5 @@
 window.ABSENSI_CONFIG = Object.freeze({
-  // Isi setelah Google Apps Script selesai di-deploy sebagai Web App (/exec).
-  GAS_ENDPOINT: '',
+  GAS_ENDPOINT: 'https://script.google.com/macros/s/AKfycbx1UR1AH579fGoyJZHtcKlhkOIbJFSboNUjwx3vbtEOxHxv_VtpyYJJj8eEb4mY01o2/exec',
   HOST_LABEL: 'djarotsantoso2@gmail.com',
   APP_VERSION: '1.5.0'
 });
